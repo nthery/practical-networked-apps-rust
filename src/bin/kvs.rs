43aa571dc31c36f38a0be84aa73fc4d25e1832b0
Nicolas Thery <nicolas.thery@gmail.com>
2019-07-10 16:50:18 +0200

    rustfmt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,4 +1,4 @@
-use clap::{App,Arg,SubCommand,AppSettings};
+use clap::{App, AppSettings, Arg, SubCommand};
 
 fn main() {
     let _matches = App::new("kvs")
@@ -6,21 +6,13 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .setting(AppSettings::SubcommandRequired)
-        .subcommand(SubCommand::with_name("get")
-                    .arg(Arg::with_name("key")
-                         .required(true)
-                         .index(1)))
-        .subcommand(SubCommand::with_name("set")
-                    .arg(Arg::with_name("key")
-                         .required(true)
-                         .index(1))
-                    .arg(Arg::with_name("value")
-                         .required(true)
-                         .index(2)))
-        .subcommand(SubCommand::with_name("rm")
-                    .arg(Arg::with_name("key")
-                         .required(true)
-                         .index(1)))
+        .subcommand(SubCommand::with_name("get").arg(Arg::with_name("key").required(true).index(1)))
+        .subcommand(
+            SubCommand::with_name("set")
+                .arg(Arg::with_name("key").required(true).index(1))
+                .arg(Arg::with_name("value").required(true).index(2)),
+        )
+        .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("key").required(true).index(1)))
         .get_matches();
 
     panic!("unimplemented");
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 use std::collections::HashMap;
 
 pub struct KvStore {
-    items: HashMap<String, String>
+    items: HashMap<String, String>,
 }
 
 // TODO: Most methods take String arguments because tests use str::to_owned().  There
 // must be a better way.
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore{ items: HashMap::new() }
+        KvStore {
+            items: HashMap::new(),
+        }
     }
 
     pub fn set(&mut self, key: String, value: String) {
