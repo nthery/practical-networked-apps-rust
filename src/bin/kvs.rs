a255361e7d76cbc8e459a31ec7453863d95e202e
Nicolas Thery <nicolas.thery@gmail.com>
2019-07-10 16:48:37 +0200

    all tests pass

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,9 +1,7 @@
 use clap::{App,Arg,SubCommand,AppSettings};
 
-use kvs::KvStore;
-
 fn main() {
-    let matches = App::new("kvs")
+    let _matches = App::new("kvs")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
@@ -25,28 +23,5 @@ fn main() {
                          .index(1)))
         .get_matches();
 
-    let mut store = KvStore::new();
-
-    match matches.subcommand() {
-        ("get", Some(smatches)) => {
-            if let Some(val) = store.get(smatches.value_of("key").unwrap().to_owned()) {
-                println!("{}", val);
-            } else {
-                eprintln!("unknown key");
-                // TODO: exit with error
-            }
-        }
-        ("set", Some(smatches)) => {
-            store.set(
-                smatches.value_of("key").unwrap().to_owned(),
-                smatches.value_of("value").unwrap().to_owned()
-            );
-        }
-        ("rm", Some(smatches)) => {
-            store.remove(
-                smatches.value_of("key").unwrap().to_owned()
-            );
-        }
-        _ => unimplemented!()
-    }
+    panic!("unimplemented");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,25 @@
-pub struct KvStore;
+use std::collections::HashMap;
 
+pub struct KvStore {
+    items: HashMap<String, String>
+}
+
+// TODO: Most methods take String arguments because tests use str::to_owned().  There
+// must be a better way.
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore{}
+        KvStore{ items: HashMap::new() }
     }
 
-    pub fn set(&mut self, _key: String, _value: String) {
-        panic!("unimplemented")
+    pub fn set(&mut self, key: String, value: String) {
+        self.items.insert(key, value);
     }
 
-    pub fn get(&self, _key: String) -> Option<String> {
-        panic!("unimplemented")
+    pub fn get(&self, key: String) -> Option<String> {
+        self.items.get(&key).map(|val| val.to_owned())
     }
 
-    pub fn remove(&mut self, _key: String) {
-        panic!("unimplemented")
+    pub fn remove(&mut self, key: String) {
+        self.items.remove(&key);
     }
 }
