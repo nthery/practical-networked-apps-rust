cc77842fe189842b687e08014381e38c057dd4d0
Nicolas Thery <nicolas.thery@gmail.com>
2019-07-13 15:24:42 +0200

    basic set implemented

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,7 +1,9 @@
 use clap::{App, AppSettings, Arg, SubCommand};
+use kvs::{KvStore, Result};
+use std::error::Error;
 
-fn main() {
-    let _matches = App::new("kvs")
+fn try_main() -> Result<()> {
+    let matches = App::new("kvs")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
@@ -15,5 +17,44 @@ fn main() {
         .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("key").required(true).index(1)))
         .get_matches();
 
-    panic!("unimplemented");
+    let mut store = KvStore::open("kv.db")?;
+
+    match matches.subcommand() {
+        ("get", Some(_smatches)) => {
+            /*
+            if let Some(val) = store.get(smatches.value_of("key").unwrap().to_owned()) {
+                println!("{}", val);
+            } else {
+                eprintln!("unknown key");
+                // TODO: exit with error
+            }
+            */
+            unimplemented!()
+        }
+        ("set", Some(smatches)) => store.set(
+            smatches.value_of("key").unwrap().to_owned(),
+            smatches.value_of("value").unwrap().to_owned(),
+        ),
+        ("rm", Some(_smatches)) => {
+            /*
+            store.remove(
+                smatches.value_of("key").unwrap().to_owned()
+                );
+            */
+            unimplemented!()
+        }
+        _ => panic!("clap should have detected missing subcommand"),
+    }
+}
+
+fn main() {
+    if let Err(err) = try_main() {
+        eprintln!("{}", err);
+        let mut src_opt = err.source();
+        while let Some(src) = src_opt {
+            eprintln!("caused by: {}", src);
+            src_opt = src.source();
+        }
+        std::process::exit(1);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,66 @@
-use std::collections::HashMap;
-use std::path::Path;
+use std::fmt;
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
-pub enum Error { Dummy }
+pub enum Error {
+    IoError(PathBuf, io::Error),
+    SerdeError(serde_json::Error),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Error::IoError(ref pathbuf, _) => write!(f, "kvs i/o error on {}", pathbuf.display()),
+            Error::SerdeError(_) => write!(f, "kvs serialization error"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            Error::IoError(_, ref err) => Some(err),
+            Error::SerdeError(ref err) => Some(err),
+        }
+    }
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Default)]
 pub struct KvStore {
-    items: HashMap<String, String>,
+    filename: PathBuf,
+    file: File,
 }
 
 // TODO: Most methods take String arguments because tests use str::to_owned().  There
 // must be a better way.
 impl KvStore {
-    pub fn new() -> KvStore {
-        KvStore::default()
-    }
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<KvStore> {
+        let pathbuf = path.as_ref().to_path_buf();
+        let file = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .create(true)
+            .open(path.as_ref())
+            .map_err(|err| Error::IoError(pathbuf.clone(), err))?;
 
-    pub fn open(_path: &Path) -> Result<KvStore> {
-        unimplemented!();
+        Ok(KvStore {
+            filename: pathbuf,
+            file: file,
+        })
     }
 
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        self.items.insert(key, value);
-        Ok(())
+        serde_json::to_writer(&self.file, &(key, value)).map_err(Error::SerdeError)
     }
 
-    pub fn get(&self, key: String) -> Result<Option<String>> {
-        Ok(self.items.get(&key).map(|val| val.to_owned()))
+    pub fn get(&self, _key: String) -> Result<Option<String>> {
+        unimplemented!();
     }
 
-    pub fn remove(&mut self, key: String) -> Result<()> {
-        self.items.remove(&key);
-        Ok(())
+    pub fn remove(&mut self, _key: String) -> Result<()> {
+        unimplemented!();
     }
 }
