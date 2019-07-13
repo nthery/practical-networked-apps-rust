125ffed455775818221fd91ecad2a45e92db6489
Nicolas Thery <nicolas.thery@gmail.com>
2019-07-13 15:28:09 +0200

    rename error enum

--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,30 @@ use std::io;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
-pub enum Error {
-    IoError(PathBuf, io::Error),
-    SerdeError(serde_json::Error),
+pub enum KvError {
+    Io(PathBuf, io::Error),
+    Serde(serde_json::Error),
 }
 
-impl fmt::Display for Error {
+impl fmt::Display for KvError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
-            Error::IoError(ref pathbuf, _) => write!(f, "kvs i/o error on {}", pathbuf.display()),
-            Error::SerdeError(_) => write!(f, "kvs serialization error"),
+            KvError::Io(ref pathbuf, _) => write!(f, "kvs i/o error on {}", pathbuf.display()),
+            KvError::Serde(_) => write!(f, "kvs serialization error"),
         }
     }
 }
 
-impl std::error::Error for Error {
+impl std::error::Error for KvError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match *self {
-            Error::IoError(_, ref err) => Some(err),
-            Error::SerdeError(ref err) => Some(err),
+            KvError::Io(_, ref err) => Some(err),
+            KvError::Serde(ref err) => Some(err),
         }
     }
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T> = std::result::Result<T, KvError>;
 
 pub struct KvStore {
     filename: PathBuf,
@@ -44,7 +44,7 @@ impl KvStore {
             .append(true)
             .create(true)
             .open(path.as_ref())
-            .map_err(|err| Error::IoError(pathbuf.clone(), err))?;
+            .map_err(|err| KvError::Io(pathbuf.clone(), err))?;
 
         Ok(KvStore {
             filename: pathbuf,
@@ -53,7 +53,7 @@ impl KvStore {
     }
 
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        serde_json::to_writer(&self.file, &(key, value)).map_err(Error::SerdeError)
+        serde_json::to_writer(&self.file, &(key, value)).map_err(KvError::Serde)
     }
 
     pub fn get(&self, _key: String) -> Result<Option<String>> {
