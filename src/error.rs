5a078188534076f738920ee988077e39a860a8af
Nicolas Thery <nicolas.thery@gmail.com>
2019-07-29 17:11:22 +0200

    extract error module

---
+++ src/error.rs
@@ -0,0 +1,65 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::convert::From;
+use std::fmt;
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, prelude::*, BufReader, BufWriter, ErrorKind, SeekFrom};
+use std::net;
+use std::path::{Path, PathBuf};
+use tempfile::NamedTempFile;
+
+// TODO: encapsulate in struct storing what operation failed (set...)?
+#[derive(Debug)]
+pub enum KvError {
+    Io(io::Error),
+    Serde(serde_json::Error),
+    KeyNotFound(String),
+    Server(String),
+    Other(Box<dyn std::error::Error>),
+}
+
+impl From<serde_json::Error> for KvError {
+    fn from(err: serde_json::Error) -> KvError {
+        KvError::Serde(err)
+    }
+}
+
+impl From<io::Error> for KvError {
+    fn from(err: io::Error) -> KvError {
+        KvError::Io(err)
+    }
+}
+
+impl From<net::AddrParseError> for KvError {
+    fn from(err: net::AddrParseError) -> KvError {
+        KvError::Other(Box::new(err))
+    }
+}
+
+impl fmt::Display for KvError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            KvError::Io(_) => write!(f, "I/O error"),
+            KvError::Serde(_) => write!(f, "Serialization error"),
+            KvError::KeyNotFound(ref key) => write!(f, "Key not found: {}", key),
+            KvError::Server(ref msg) => write!(f, "Server error: {}", msg),
+            KvError::Other(ref err) => write!(f, "Other error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for KvError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            KvError::Io(ref err) => Some(err),
+            KvError::Serde(ref err) => Some(err),
+            KvError::KeyNotFound(_) => None,
+            KvError::Server(_) => None,
+            KvError::Other(ref err) => err.source(),
+        }
+    }
+}
+
+pub type Result<T> = std::result::Result<T, KvError>;
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-// TODO: break down this file
-
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::convert::From;
@@ -10,63 +8,13 @@ use std::net;
 use std::path::{Path, PathBuf};
 use tempfile::NamedTempFile;
 
-pub struct KvsEngine;
-
 pub mod wire;
+mod error;
 
-// TODO: encapsulate in struct storing what operation failed (set...)?
-#[derive(Debug)]
-pub enum KvError {
-    Io(io::Error),
-    Serde(serde_json::Error),
-    KeyNotFound(String),
-    Server(String),
-    Other(Box<dyn std::error::Error>),
-}
+pub use error::KvError;
+pub use error::Result;
 
-impl From<serde_json::Error> for KvError {
-    fn from(err: serde_json::Error) -> KvError {
-        KvError::Serde(err)
-    }
-}
-
-impl From<io::Error> for KvError {
-    fn from(err: io::Error) -> KvError {
-        KvError::Io(err)
-    }
-}
-
-impl From<net::AddrParseError> for KvError {
-    fn from(err: net::AddrParseError) -> KvError {
-        KvError::Other(Box::new(err))
-    }
-}
-
-impl fmt::Display for KvError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            KvError::Io(_) => write!(f, "I/O error"),
-            KvError::Serde(_) => write!(f, "Serialization error"),
-            KvError::KeyNotFound(ref key) => write!(f, "Key not found: {}", key),
-            KvError::Server(ref msg) => write!(f, "Server error: {}", msg),
-            KvError::Other(ref err) => write!(f, "Other error: {}", err),
-        }
-    }
-}
-
-impl std::error::Error for KvError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match *self {
-            KvError::Io(ref err) => Some(err),
-            KvError::Serde(ref err) => Some(err),
-            KvError::KeyNotFound(_) => None,
-            KvError::Server(_) => None,
-            KvError::Other(ref err) => err.source(),
-        }
-    }
-}
-
-pub type Result<T> = std::result::Result<T, KvError>;
+pub struct KvsEngine;
 
 type Index = HashMap<String, u64>;
 
