4dc4b2d3b4c31fbd59a89a38348fb5f1f59547d9
Nicolas Thery <nthery@gmail.com>
2019-08-25 15:43:35 +0200

    allow single KvsClient object to send N requests

--- src/client.rs
+++ src/client.rs
@@ -7,53 +7,45 @@ use std::net::TcpStream;
 
 /// TCP/IP client connecting to key-value store server.
 pub struct KvsClient {
-    stream: TcpStream,
+    addr: SocketAddr,
 }
 
 impl KvsClient {
     /// Creates a new client connected to server at `addr`.
     pub fn new(addr: SocketAddr) -> Result<KvsClient> {
-        Ok(KvsClient {
-            stream: TcpStream::connect(addr)?,
-        })
+        Ok(KvsClient { addr })
     }
 
     pub fn get(&mut self, key: &str) -> Result<Option<String>> {
-        debug!("C: get({})", key);
-        let req = serde_json::to_string(&wire::Request::Get(key.to_string()))?;
-        writeln!(self.stream, "{}", req)?;
-        let reply = serde_json::from_reader::<_, wire::Reply>(&mut self.stream)?;
-        debug!("C: result: {:?}", reply);
-        reply.0.map_err(KvError::Server)
+        self.send_recv(wire::Request::Get(key.to_string()))
     }
 
     pub fn set(&mut self, key: &str, val: &str) -> Result<()> {
-        debug!("C: set({}, {})", key, val);
-        let req = serde_json::to_string(&wire::Request::Set(key.to_string(), val.to_string()))?;
-        writeln!(self.stream, "{}", req)?;
-        let reply = serde_json::from_reader::<_, wire::Reply>(&mut self.stream)?;
-        debug!("C: result: {:?}", reply);
-        reply.0.map(|_| ()).map_err(KvError::Server)
+        self.send_recv(wire::Request::Set(key.to_string(), val.to_string()))
+            .map(|_| ())
     }
 
     pub fn rm(&mut self, key: &str) -> Result<()> {
-        debug!("C: rm({})", key);
-        let req = serde_json::to_string(&wire::Request::Rm(key.to_string()))?;
-        writeln!(self.stream, "{}", req)?;
-        let reply = serde_json::from_reader::<_, wire::Reply>(&mut self.stream)?;
-        debug!("C: result: {:?}", reply);
-        reply.0.map(|_| ()).map_err(KvError::Server)
+        self.send_recv(wire::Request::Rm(key.to_string()))
+            .map(|_| ())
     }
 
     /// Requests server to stop.
     ///
     /// When this function returns, the server has stopped all processing.
     pub fn shutdown(&mut self) -> Result<()> {
-        debug!("C: shutdown");
-        let req = serde_json::to_string(&wire::Request::Shutdown)?;
-        writeln!(self.stream, "{}", req)?;
-        let reply = serde_json::from_reader::<_, wire::Reply>(&mut self.stream)?;
-        debug!("C: result: {:?}", reply);
-        reply.0.map(|_| ()).map_err(KvError::Server)
+        self.send_recv(wire::Request::Shutdown).map(|_| ())
+    }
+
+    /// Sends request `req` to server and waits for reply.
+    fn send_recv(&self, req: wire::Request) -> Result<Option<String>> {
+        debug!("C: sending {:?}", req);
+        // A socket is a vehicle for a single request and so must be created per-request.
+        let mut stream = TcpStream::connect(self.addr)?;
+        let ser_req = serde_json::to_string(&req)?;
+        writeln!(stream, "{}", ser_req)?;
+        let reply = serde_json::from_reader::<_, wire::Reply>(&mut stream)?;
+        debug!("C: received: {:?}", reply);
+        reply.0.map_err(KvError::Server)
     }
 }
--- tests/client_server.rs
+++ tests/client_server.rs
@@ -7,10 +7,25 @@ fn shutdown() {
     let tmpdir = TempDir::new().unwrap();
     let engine = KvStore::open(&tmpdir).unwrap();
     let pool = SharedQueueThreadPool::new(1).unwrap();
-    let addr = "127.0.0.1:4000".parse::<SocketAddr>().unwrap();
+    let addr = "127.0.0.1:5000".parse::<SocketAddr>().unwrap();
     let mut server = KvsServer::new(engine, pool, addr).unwrap();
     let mut client = KvsClient::new(addr).unwrap();
     let server_thread = std::thread::spawn(move || server.run());
+    client.shutdown().unwrap();
+    assert!(server_thread.join().is_ok());
+}
+
+#[test]
+fn several_operations_from_single_client() {
+    let tmpdir = TempDir::new().unwrap();
+    let engine = KvStore::open(&tmpdir).unwrap();
+    let pool = SharedQueueThreadPool::new(1).unwrap();
+    let addr = "127.0.0.1:5001".parse::<SocketAddr>().unwrap();
+    let mut server = KvsServer::new(engine, pool, addr).unwrap();
+    let mut client = KvsClient::new(addr).unwrap();
+    let server_thread = std::thread::spawn(move || server.run());
+    client.set("K1", "V1").unwrap();
+    assert_eq!(client.get("K1").unwrap(), Some("V1".to_string()));
     client.shutdown().unwrap();
     assert!(server_thread.join().is_ok());
 }
